f6b42accc4c1463171e4f40b691ed197aba4f912
Daniel Waiguru <danielirungu91@gmail.com>
2021-06-10 14:49:13 +0300

    Added NDK to secure APIKEY

--- data-remote/src/main/java/tech/danielwaiguru/data/api/MovieApiService.kt
+++ data-remote/src/main/java/tech/danielwaiguru/data/api/MovieApiService.kt
@@ -3,21 +3,21 @@ package tech.danielwaiguru.data.api
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
-import tech.danielwaiguru.data.common.Constants.API_KEY
+import tech.danielwaiguru.data.common.Constants.getApiKey
 import tech.danielwaiguru.data.models.MovieDto
 import tech.danielwaiguru.data.models.response.MovieResponse
 
 interface MovieApiService {
     @GET("movie/popular")
     suspend fun getPopularMovies(
-        @Query("api_key") apiKey: String = API_KEY,
+        @Query("api_key") apiKey: String = getApiKey(),
         @Query("language") language: String = "en-US",
         @Query("page") page: Int = 1
     ): MovieResponse
     @GET("movie/{movie_id}")
     suspend fun getMovieDetails(
             @Path("movie_id") mId: Int,
-            @Query("api_key") apiKey: String = API_KEY,
+            @Query("api_key") apiKey: String = getApiKey(),
             @Query("language") language: String = "en-US",
     ): MovieDto
 }
--- data-remote/src/main/java/tech/danielwaiguru/data/common/Constants.kt
+++ data-remote/src/main/java/tech/danielwaiguru/data/common/Constants.kt
@@ -1,6 +1,9 @@
 package tech.danielwaiguru.data.common
 
 object Constants {
+    init {
+        System.loadLibrary("native-lib")
+    }
     const val BASE_URL = "https://api.themoviedb.org/3/"
-    const val API_KEY = "7d533c39f66d4a212a1b3c81273ec7d1"
+    external fun getApiKey(): String
 }
